723925ba4d1403a76d7c240d4f699208f95abef8
RizwanSuvo99 <rizwanshuvo@gmail.com>
2023-11-27 18:48:30 +0600

    "Local storage added"

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProductsSelect from "../../component/ProductsSelect/ProductsSelect";
 import ProductsShow from "../../component/ProductsShow/ProductsShow";
 import "./App.css";
 
 function App() {
   const [products, setProducts] = useState([]);
+
+  //Data get from local storage
+  useEffect(() => {
+    localStorage.setItem("products", JSON.stringify(products));
+  }, [products]);
 
   return (
     <main>
